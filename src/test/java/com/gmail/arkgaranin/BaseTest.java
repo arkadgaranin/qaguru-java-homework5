e28c5ade02e38701c21cc9592e91368ebe98935d
Arkadiy <arkadiy.garanin@socialsys.ru>
2021-10-02 15:01:38 +0300

    Создал класс BaseTest

---
+++ src/test/java/com/gmail/arkgaranin/BaseTest.java
@@ -0,0 +1,14 @@
+package com.gmail.arkgaranin;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class BaseTest {
+
+  @BeforeAll
+  static void setup() {
+    Configuration.startMaximized = true;
+    Configuration.browser = "chrome";
+    Configuration.baseUrl = "https://demoqa.com";
+  }
+}
