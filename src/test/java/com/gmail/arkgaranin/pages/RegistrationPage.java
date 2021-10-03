55220717882c52794df318b3cbe7b781d90df279
Arkadiy <arkadiy.garanin@socialsys.ru>
2021-10-03 21:50:55 +0300

    Добавил цикл в метод проверки данных в попапе регистрации

--- src/test/java/com/gmail/arkgaranin/pages/RegistrationPage.java
+++ src/test/java/com/gmail/arkgaranin/pages/RegistrationPage.java
@@ -1,9 +1,12 @@
 package com.gmail.arkgaranin.pages;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import com.gmail.arkgaranin.components.Calendar;
+import org.assertj.core.api.SoftAssertions;
 
 import java.io.File;
+import java.util.Map;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -101,8 +104,20 @@ public class RegistrationPage {
     resultsPopupTitle.shouldHave(text(RESULTS_TITLE));
   }
 
-  public RegistrationPage checkResultsPopupValue(String key, String value) {
-    $x("//td[text()='" + key + "']").parent().shouldHave(text(value));
-    return this;
+  public void checkResultsPopupValue(Map<String, String> expectedData) {
+    ElementsCollection popupResults = $$(".table-responsive tbody tr").snapshot();
+
+    SoftAssertions softly = new SoftAssertions();
+
+    for (SelenideElement popupResult : popupResults) {
+      String line = popupResult.$("td").text();
+      String expectedValue = expectedData.get(line);
+      String actualValue = popupResult.$("td", 1).text();
+
+      softly.assertThat(actualValue)
+          .as(format("Result in line %s was %s, but expected %s", line, actualValue, expectedValue))
+          .isEqualTo(expectedValue);
+    }
+    softly.assertAll();
   }
 }
